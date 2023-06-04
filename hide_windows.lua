344d05fe79bd1f2f64730c95f5dd7176129a7f40
Charles Chamberlain <charles@nrwhl.xyz>
2023-06-04 02:30:39 -0400

    Disable hide windows

--- hide_windows.lua
+++ hide_windows.lua
@@ -1,13 +1,13 @@
-function applicationWatcher(appName, eventType, appObject)
-  if (eventType == hs.application.watcher.activated) then
-    windows = hs.window.visibleWindows()
-    for _, window in ipairs(windows) do
-      if (appName ~= window:application():name() and window:application():name() ~= "Finder") then
-        -- hs.alert.show(window:application():name())
-        window:application():hide()
-      end
-    end
-  end
-end
-appWatcher = hs.application.watcher.new(applicationWatcher)
-appWatcher:start()
+-- function applicationWatcher(appName, eventType, appObject)
+--   if (eventType == hs.application.watcher.activated) then
+--     windows = hs.window.visibleWindows()
+--     for _, window in ipairs(windows) do
+--       if (appName ~= window:application():name() and window:application():name() ~= "Finder") then
+--         -- hs.alert.show(window:application():name())
+--         window:application():hide()
+--       end
+--     end
+--   end
+-- end
+-- appWatcher = hs.application.watcher.new(applicationWatcher)
+-- appWatcher:start()
