202f7fe9fb05f3296ee4a03e3b3e44940c7b597f
Charles Chamberlain <charles@nrwhl.xyz>
2023-06-04 03:17:53 -0400

    Use hhtwm

--- init.lua
+++ init.lua
@@ -1,7 +1,14 @@
 hs.window.animationDuration = 0.1
 
-hs.hotkey.bind({"cmd", "alt"}, "W", function() 
-    hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+hs.hotkey.bind({"ctrl", "cmd"}, "r", function() 
+  hs.reload()
+end)
+
+
+hs.hotkey.bind({"ctrl", "cmd"}, "T", function() 
+  iterm = hs.application.find("iTerm")
+  iterm:activate()
+  iterm:selectMenuItem({"Shell", "New Window"})
 end)
 
 -- Reload automatically
@@ -10,3 +17,16 @@ spoon.ReloadConfiguration:start()
 hs.alert.show("Config loaded")
 
 require 'hide_windows'
+hhtwm = require('hhtwm')
+hhtwm.margin = 20
+margin = 10
+hhtwm.screenMargin = { top = margin, bottom = margin, left = margin, right = margin }
+-- hhtwm.defaultLayouts = [ "floating" ]
+
+hhtwm.filters = {
+  { app = 'Finder', tile = false },
+  { app = 'Hammerspoon', title = 'Hammerspoon Console', tile = false },
+  { app = 'The Archive', tile = false },
+}
+hhtwm.start()
+hhtwm.setLayout("main-left")
